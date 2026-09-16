6f68bc32959f71994fe45de578595e0f8a46a584
Tanaya Asalkar <145457627+tanayaasalkar1@users.noreply.github.com>
2026-09-16 20:01:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+let form = document.getElementById('form');
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+let isbn = document.getElementById('isbn');
+let button = document.getElementById('submit');
+
+let table = document.getElementById('table');
+let tbody = document.getElementById('book-list');
+
+button.addEventListener('click', (e) => {
+	e.preventDefault();
+	const row = document.createElement('tr');
+
+	row.innerHTML = `
+	  <td>${title.value}</td>
+        <td>${author.value}</td>
+        <td>${isbn.value}</td>
+		<td> <button class="delete">Clear</button> </td>
+	`
+	tbody.appendChild(row);
+	form.reset();
+	
+})
+
+tbody.addEventListener("click", function(event) { 
+ if (event.target.classList.contains("delete")) { 
+ const row = event.target.parentElement.parentElement; 
+row.remove(); 
+ } 
+});
+
+
+
+
+
+
+
